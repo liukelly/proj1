6b4317adb521b86f04d8f31e4cf5cfd6573ee028
Kelly Liu <kelly.liu@berkeley.edu>
2016-03-18 13:03:56 -0700

    add pokemon stuff

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,12 @@
+class PokemonsController < ApplicationController
+  # before_filter :authenticate_trainer!
+
+  # def index
+  #   @trainers = Trainer.all
+  # end
+
+  # def show
+  #   @trainer = Trainer.find(params[:id])
+  # end
+
+end
